940fa2b9877744b376dcb2303e1a5bfc4d06c796
zyf <zyf034100@gmail.com>
2017-01-24 14:47:47 +0800

    bearychat-hubot 更新修复 bug

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # export HUBOT_LOG_LEVEL="debug"
-EXPRESS_PORT=8080 HUBOT_BEARYCHAT_TOKENS=198a4575a9851abdfb8a95e4e28ae76c ./bin/hubot -a bearychat > log 2>&1
+# export EXPRESS_PORT=8080
+# export HUBOT_BEARYCHAT_MODE=http
+export HUBOT_BEARYCHAT_TOKENS=198a4575a9851abdfb8a95e4e28ae76c 
+./bin/hubot -a bearychat > log 2>&1
