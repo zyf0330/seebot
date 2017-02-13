0ae6ff9f9969bc22621868d340c211bb20250b57
zyf <zyf@server-linux.lan>
2017-02-13 10:01:05 +0800

    修正脚本错误输出的语法问题

--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
 export HUBOT_LOG_LEVEL="debug"
-bin/hubot >>debuglog 2>>&1
+bin/hubot >>debuglog 2>&1
--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export HUBOT_LOG_LEVEL="debug"
+# export HUBOT_LOG_LEVEL="debug"
 # export EXPRESS_PORT=8080 
 # export HUBOT_BEARYCHAT_MODE=http
 export HUBOT_BEARYCHAT_TOKENS=198a4575a9851abdfb8a95e4e28ae76c 
